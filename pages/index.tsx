00af066ee81ec6c2a27c7f74d003664abe1595c6
mateusoliiveira <teusolive@gmail.com>
2023-01-06 13:40:11 -0300

    fix II

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,6 @@ import type { GetServerSidePropsContext, NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import Banner from "../public/banner.jpg";
-import { AiFillDollarCircle, AiFillBank } from "react-icons/ai";
-import { RiLuggageDepositFill } from "react-icons/ri";
-import Schedule from "../components/Schedule";
 
 const Home: NextPage<{ data: any }> = ({ data }: { data: any }) => {
   return (
@@ -37,51 +34,8 @@ const Home: NextPage<{ data: any }> = ({ data }: { data: any }) => {
                 }
                 className="w-2/3 lg:w-1/3 uppercase shadow-lg text-2xl font-bold tracking-wide bg-orange-600 text-gray-100 p-3 rounded-lg focus:outline-none focus:shadow-outline hover:bg-green-500 hover:scale-105 transition-all"
               >
-                FAÇA SEU CADASTRO
+                FAÇA SEU CADASTRO E RECEBA SEU BÔNUS
               </button>
-            </div>
-            <div className="flex flex-wrap -m-4">
-              <div className="xl:w-1/3 md:w-1/2 p-4">
-                <div className="border border-gray-200 p-6 rounded-lg bg-slate-100">
-                  <div className="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-orange-500 mb-4">
-                    <AiFillDollarCircle size="30" />
-                  </div>
-                  <h2 className="text-lg text-gray-900 font-medium title-font mb-2">
-                    Bônus sem Rollover
-                  </h2>
-                  <p className="leading-relaxed text-base">
-                    Se a sua primeira de R$5 for vencedora, seu saque já está
-                    liberado.
-                  </p>
-                </div>
-              </div>
-              <div className="xl:w-1/3 md:w-1/2 p-4">
-                <div className="border border-gray-200 p-6 rounded-lg bg-slate-100">
-                  <div className="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-orange-500 mb-4">
-                    <RiLuggageDepositFill size="30" />
-                  </div>
-                  <h2 className="text-lg text-gray-900 font-medium title-font mb-2">
-                    Deposite a partir de R$1
-                  </h2>
-                  <p className="leading-relaxed text-base">
-                    Aqui você começa a operar com qualquer valor, até um real!
-                  </p>
-                </div>
-              </div>
-              <div className="xl:w-1/3 md:w-1/2 p-4">
-                <div className="border border-gray-200 p-6 rounded-lg bg-slate-100">
-                  <div className="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-orange-500 mb-4">
-                    <AiFillBank size="30" />
-                  </div>
-                  <h2 className="text-lg text-gray-900 font-medium title-font mb-2">
-                    Saques e depósitos via PIX
-                  </h2>
-                  <p className="leading-relaxed text-base">
-                    Saque e depósito via PIX sem burocracia, dinheiro na sua
-                    conta instantaneamente.
-                  </p>
-                </div>
-              </div>
             </div>
           </div>
         </section>
